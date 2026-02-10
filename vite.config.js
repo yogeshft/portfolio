045e3225e5b6578ecf8c3fb6247d6b13a2e2fe1c
yogesh <yadavyogesh404@gmail.com>
2026-02-11 02:23:28 +0530

    deployment

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  // GitHub Pages: use repo name as base, unless this is a user site (username.github.io)
+  base: (() => {
+    const repo = process.env.GITHUB_REPOSITORY?.split('/')[1] ?? ''
+    const isUserPage = repo.endsWith('.github.io')
+    return repo && !isUserPage ? `/${repo}/` : '/'
+  })(),
   plugins: [react()],
 })
